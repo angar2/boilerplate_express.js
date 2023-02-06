8d8467c05ac61352a2ee9e33598eda26f4bb4228
앵가리 <>
2023-02-06 19:11:45 +0900

    회원가입 기능: body-parser

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const config = require('./config/key')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true})); // form
+app.use(bodyParser.json()); // json
+
 mongoose.set("strictQuery", false);
 mongoose.connect(config.mongoURI).then(
     () => console.log('MongoDB Connected...')).catch(
@@ -10,6 +17,14 @@ mongoose.connect(config.mongoURI).then(
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({success: true, userInfo})
+    });
 });
 
 app.listen(port, () => {
