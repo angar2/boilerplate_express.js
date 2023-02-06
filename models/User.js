3f20ad01879737b786e1c195bae3c76eb5878ea0
앵가리 <>
2023-02-06 18:37:58 +0900

    Model 생성: User

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    // 회원 타입(일반: 0, 관리자: 1)
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+const User = mongoose.model('User',userSchema)
+
+module.exports = { User }
