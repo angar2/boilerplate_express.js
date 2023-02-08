215c7f78284fd9c020e7b0d1f1466b22dd5ad5d3
앵가리 <>
2023-02-08 20:15:51 +0900

    Redux 세팅

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// combineReducers: 여러개의 reducer를 하나로 합쳐주는 기능 -> rootReducer
+import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    user
+});
+
+export default rootReducer;
