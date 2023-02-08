58f8348bca78fcf9298f280629bccd66f0a003c4
앵가리 <>
2023-02-08 18:25:26 +0900

    Server-Client API: axios, http-proxy-middleware

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 function LandingPage() {
+    useEffect(() => {
+        axios.get('/')
+        .then(res => console.log(res))
+    }, []);
   return (
     <div>LandingPage</div>
-  )
-}
+  );
+};
 
 export default LandingPage
