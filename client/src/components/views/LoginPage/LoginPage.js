1584329d4affe79891aaace70d28b070de82206e
앵가리 <>
2023-02-09 15:17:29 +0900

    로그인 기능: redux 활용

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
+
+function LoginPage(props) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
@@ -12,12 +18,28 @@ function LoginPage() {
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value);
   };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    let body = {
+      email: Email,
+      password: Password
+    };
+    dispatch(loginUser(body))
+      .then(res => {
+        if(res.payload.success) {
+          navigate('/')
+        } else {
+          alert('Error')
+        }
+      });
+  };
   
   return (
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
     }}>
-      <form style={{display: 'flex', flexDirection: 'column'}}>
+      <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={onSubmitHandler}>
         <label>Email</label>
         <input type='email' value={Email} onChange={onEmailHandler} />
         <label>Passoword</label>
