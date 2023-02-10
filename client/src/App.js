66728cd180e32b3dba6ccbea55ff612308382e65
앵가리 <>
2023-02-10 17:38:23 +0900

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LandingPage from "./components/views/LandingPage/LandingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
 import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 import NavBar from "./components/views/NavBar/NavBar";
+import Footer from "./components/views/Footer/Footer";
 import Auth from './hoc/auth';
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route path="/register" element={<AuthRegisterPage />}/>
         </Routes>
       </div>
+      <Footer />
     </Router>
   );
 };
